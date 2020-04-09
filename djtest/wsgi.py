bf277503cc43173f4b6733d4d3eb4a689bb37743
Yo <pfcfia@gmail.com>
2020-04-09 20:52:06 +0800

    complete

--- djtest/wsgi.py
+++ djtest/wsgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+#..- django <..> apache
+from os.path import join,dirname,abspath
+import sys
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0,PROJECT_DIR)
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djtest.settings')
 
 application = get_wsgi_application()
+
